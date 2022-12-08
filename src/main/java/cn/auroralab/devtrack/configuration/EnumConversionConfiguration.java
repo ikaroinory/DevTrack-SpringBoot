e4049a64a1342ab53562d9e67c0b3a2616a34fa0
阿夢 <ikaroinory233@gmail.com>
2022-12-08 16:52:18 +0800

    feat: 完善枚举转换配置

--- src/main/java/cn/auroralab/devtrack/configuration/EnumConversionConfiguration.java
+++ src/main/java/cn/auroralab/devtrack/configuration/EnumConversionConfiguration.java
@@ -1,6 +1,9 @@
 package cn.auroralab.devtrack.configuration;
 
 import cn.auroralab.devtrack.enumeration.Gender;
+import cn.auroralab.devtrack.enumeration.Priority;
+import cn.auroralab.devtrack.enumeration.SourceOfDemand;
+import cn.auroralab.devtrack.enumeration.TaskType;
 import cn.auroralab.devtrack.util.Parseable;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
@@ -13,5 +16,8 @@ public class EnumConversionConfiguration implements WebMvcConfigurer {
     public void addFormatters(@NonNull FormatterRegistry registry) {
         WebMvcConfigurer.super.addFormatters(registry);
         registry.addConverter(String.class, Gender.class, source -> Parseable.parse(Integer.parseInt(source), Gender.class, Gender.OTHERS));
+        registry.addConverter(String.class, TaskType.class, source -> Parseable.parse(Integer.parseInt(source), TaskType.class, TaskType.UNKNOWN));
+        registry.addConverter(String.class, Priority.class, source -> Parseable.parse(Integer.parseInt(source), Priority.class, Priority.UNKNOWN));
+        registry.addConverter(String.class, SourceOfDemand.class, source -> Parseable.parse(Integer.parseInt(source), SourceOfDemand.class, SourceOfDemand.UNKNOWN));
     }
 }
